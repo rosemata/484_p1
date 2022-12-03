66264c7c777d2151851a26c866624bf598c7d298
Roselle Mata <roselle.tmata@gmail.com>
2022-12-03 12:45:54 -0600

    Update Avatar.js

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,30 +1,35 @@
 import { useEffect, useState } from 'react'
 import { supabase } from './supabaseClient'
 
-const MAX_COUNT = 5;
-
 export default function Avatar({ url, size, onUpload }) {
-  const [avatarUrl, setAvatarUrl] = useState(null)
+  const [avatarUrl, setAvatarUrl] = useState([])
   const [uploading, setUploading] = useState(false)
 
   const handleUploadFiles = files => {
-    const uploaded = [...avatarUrl];
-    let limitExceeded = false;
-    files.some((file) => {
-        if (uploaded.findIndex((f) => f.name === file.name) === -1) {
-            uploaded.push(file);
-            if (uploaded.length === MAX_COUNT) setUploading(true);
-            if (uploaded.length > MAX_COUNT) {
-                alert(`You can only add a maximum of ${MAX_COUNT} files`);
-                setUploading(false);
-                limitExceeded = true;
-                return true;
-            }
-        }
-    })
-    if (!limitExceeded) setAvatarUrl(uploaded)
+      const uploaded = [...avatarUrl];
+      let limitExceeded = false;
+      files.some((file) => {
+          if (uploaded.findIndex((f) => f.name === file.name) === -1) {
+              uploaded.push(file);
+              if (uploaded.length === MAX_COUNT) setUploading(true);
+              if (uploaded.length > MAX_COUNT) {
+                  alert(`You can only add a maximum of ${MAX_COUNT} files`);
+                  setUploading(false);
+                  limitExceeded = true;
+                  return true;
+              }
+          }
+      })
+      if (!limitExceeded) setAvatarUrl(uploaded)
 
-}
+  }
+
+  const handleFileEvent =  (e) => {
+      const chosenFiles = Array.prototype.slice.call(e.target.files)
+      handleUploadFiles(chosenFiles);
+  }
+
+  
 
   useEffect(() => {
     if (url) downloadImage(url)
