e54fd932149e80ef46b0fb4128ac655ba5e484e7
Roselle Mata <roselle.tmata@gmail.com>
2022-12-03 13:25:56 -0600

    Update Avatar.js

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,9 +1,14 @@
 import { useEffect, useState } from 'react'
 import { supabase } from './supabaseClient'
 
+const MAX_COUNT = 5;
+
 export default function Avatar({ url, size, onUpload }) {
-  const [avatarUrl, setAvatarUrl] = useState(null)
-  const [uploading, setUploading] = useState(false)
+  // const [avatarUrl, setAvatarUrl] = useState(null)
+  // const [uploading, setUploading] = useState(false)
+
+	const [avatarUrl, setAvatarUrl] = useState([])
+  const [uploading, setUploading] = useState(false);
 
   useEffect(() => {
     if (url) downloadImage(url)
@@ -23,30 +28,85 @@ export default function Avatar({ url, size, onUpload }) {
   }
 
   async function uploadAvatar(event) {
-    // try {
+    try {
       setUploading(true)
 
-    //   if (!event.target.files || event.target.files.length === 0) {
-    //     throw new Error('You must select an image to upload.')
-    //   }
+      if (!event.target.files || event.target.files.length === 0) {
+        throw new Error('You must select an image to upload.')
+      }
 
       const file = event.target.files[0]
       const fileExt = file.name.split('.').pop()
       const fileName = `${Math.random()}.${fileExt}`
       const filePath = `${fileName}`
 
-    //   let { error: uploadError } = await supabase.storage.from('avatars').upload(filePath, file)
+      let { error: uploadError } = await supabase.storage.from('avatars').upload(filePath, file)
 
-    //   if (uploadError) {
-    //     throw uploadError
-    //   }
+      if (uploadError) {
+        throw uploadError
+      }
 
       onUpload(filePath)
-    // } catch (error) {
-    //   alert(error.message)
-    // } finally {
-    //   setUploading(false)
-    // }
+    } catch (error) {
+      alert(error.message)
+    } finally {
+      setUploading(false)
+    }
+  }
+
+  // return (
+  //   <div>
+  //     {avatarUrl ? (
+  //       <img
+  //         src={avatarUrl}
+  //         alt="Avatar"
+  //         className="avatar image"
+  //         style={{ height: size, width: size }}
+  //       />
+  //     ) : (
+  //       <div className="avatar no-image" style={{ height: size, width: size }} />
+  //     )}
+  //     <div style={{ width: size }}>
+  //       <label className="button primary block" htmlFor="single">
+  //         {uploading ? 'Uploading ...' : 'Upload'}
+  //       </label>
+  //       <input
+  //         style={{
+  //           visibility: 'hidden',
+  //           position: 'absolute',
+  //         }}
+  //         type="file"
+  //         id="single"
+  //         accept="image/*"
+  //         onChange={uploadAvatar}
+  //         disabled={uploading}
+  //       />
+  //     </div>
+  //   </div>
+  // )
+
+  const handleUploadFiles = files => {
+      const uploaded = [...avatarUrl];
+      let limitExceeded = false;
+      files.some((file) => {
+          if (uploaded.findIndex((f) => f.name === file.name) === -1) {
+              uploaded.push(file);
+              if (uploaded.length === MAX_COUNT) setUploading(true);
+              if (uploaded.length > MAX_COUNT) {
+                  alert(`You can only add a maximum of ${MAX_COUNT} files`);
+                  setUploading(false);
+                  limitExceeded = true;
+                  return true;
+              }
+          }
+      })
+      if (!limitExceeded) setAvatarUrl(uploaded)
+
+  }
+
+  const handleFileEvent =  (e) => {
+      const chosenFiles = Array.prototype.slice.call(e.target.files)
+      handleUploadFiles(chosenFiles);
   }
 
   return (
@@ -77,6 +137,30 @@ export default function Avatar({ url, size, onUpload }) {
           disabled={uploading}
         />
       </div>
+    {/* </div>
+
+
+  <div className="App"> */}
+
+    <input id='fileUpload' type='file' multiple
+        accept='application/pdf, image/png'
+                  onChange={handleFileEvent}
+                  disabled={uploading}
+    />
+
+    <label htmlFor='fileUpload'>
+      <a  className={`btn btn-primary ${!uploading ? '' : 'disabled' } `}>Upload Files</a>
+    </label>
+
+    <div className="uploaded-files-list">
+      {avatarUrl.map(file => (
+                  <div >
+                      {file.name}
+                  </div>
+              ))}
     </div>
-  )
+
+  </div>
+);
+
 }
