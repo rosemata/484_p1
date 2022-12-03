3e9c92330d4c2935748e8a9d909099aa17d45c03
Roselle Mata <roselle.tmata@gmail.com>
2022-12-03 13:17:17 -0600

    Update Avatar.js

--- src/Avatar.js
+++ src/Avatar.js
@@ -30,10 +30,10 @@ export default function Avatar({ url, size, onUpload }) {
     //     throw new Error('You must select an image to upload.')
     //   }
 
-    //   const file = event.target.files[0]
-    //   const fileExt = file.name.split('.').pop()
-    //   const fileName = `${Math.random()}.${fileExt}`
-    //   const filePath = `${fileName}`
+      const file = event.target.files[0]
+      const fileExt = file.name.split('.').pop()
+      const fileName = `${Math.random()}.${fileExt}`
+      const filePath = `${fileName}`
 
     //   let { error: uploadError } = await supabase.storage.from('avatars').upload(filePath, file)
 
