d255e8878a477d81448ab3f67b885968e1632ffc
mliew2 <mliew2@uic.edu>
2022-12-03 20:57:18 -0600

    Update algoliaClient.js

--- src/algoliaClient.js
+++ src/algoliaClient.js
@@ -1,6 +1,6 @@
 import algoliasearch from 'algoliasearch/lite';
 
-const algoliaAppId = process.env.ALGOLIA_APP_ID
-const algoliaSearchOnlyAPIKey = process.env.ALGOLIA_API_KEY
+const algoliaAppId = process.env.REACT_APP_ALGOLIA_APP_ID
+const algoliaSearchOnlyAPIKey = process.env.REACT_APP_ALGOLIA_API_KEY
 
 export const searchClient = algoliasearch(algoliaAppId, algoliaSearchOnlyAPIKey)
