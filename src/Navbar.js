9a3cf900ec6422fca4e08c566001334f0781cf3c
mliew2 <mliew2@uic.edu>
2022-12-04 17:48:50 -0600

    Add sign out button to top

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { supabase } from './supabaseClient'
 
 export default function Nav({ session }) {
     if (!session){
@@ -17,6 +18,9 @@ export default function Nav({ session }) {
                 <button><NavLink to="/search">Search</NavLink></button>
                 <button><NavLink to="/account">Update Profile</NavLink></button>
                 <button><NavLink to="/upload">Upload a New Note!</NavLink></button>
+                <button className="button block" onClick={() => supabase.auth.signOut()}>
+                    Sign Out
+                </button>
             </div>
         )
     }
