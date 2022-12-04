8758ca401f7b7c3c713166e9058d205a44e3039c
Roselle Mata <roselle.tmata@gmail.com>
2022-12-03 21:35:56 -0600

    Update Upload.js
    tessaract

--- src/Upload.js
+++ src/Upload.js
@@ -10,6 +10,11 @@ export default function Upload({ session }) {
     const [avatar_url, setAvatarUrl] = useState(null)
     const [note_id, setNoteId] = useState(null)
     const navigate = useNavigate();
+    
+    const [isLoading, setIsLoading] = React.useState(false);
+    const [image, setImage] = React.useState('');
+    const [text, setText] = React.useState('');
+    const [progress, setProgress] = React.useState(0);
 
     useEffect(() => {
         if(note_id != null) {
@@ -21,6 +26,26 @@ export default function Upload({ session }) {
             })
         }
     }, [note_id])
+
+    const handleSubmit = () => {
+      setIsLoading(true);
+      Tesseract.recognize(image, 'eng', {
+        logger: (m) => {
+          console.log(m);
+          if (m.status === 'recognizing text') {
+            setProgress(parseInt(m.progress * 100));
+          }
+        },
+      })
+        .catch((err) => {
+          console.error(err);
+        })
+        .then((result) => {
+          console.log(result.data);
+          setText(result.data.text);
+          setIsLoading(false);
+        });
+    };
 
     async function uploadNote({ title, content, avatar_url }) {
         try {
@@ -92,6 +117,52 @@ export default function Upload({ session }) {
             {loading ? 'Loading ...' : 'Add note!'}
             </button>
         </div>
+
+        <div className="container" style={{ height: '100vh' }}>
+      <div className="row h-100">
+        <div className="col-md-5 mx-auto h-100 d-flex flex-column justify-content-center">
+          {!isLoading && (
+            <h1 className="text-center py-5 mc-5">Image To Text</h1>
+          )}
+          {isLoading && (
+            <>
+              <progress className="form-control" value={progress} max="100">
+                {progress}%{' '}
+              </progress>{' '}
+              <p className="text-center py-0 my-0">Converting:- {progress} %</p>
+            </>
+          )}
+          {!isLoading && !text && (
+            <>
+              <input
+                type="file"
+                onChange={(e) =>
+                  setImage(URL.createObjectURL(e.target.files[0]))
+                }
+                className="form-control mt-5 mb-2"
+              />
+              <input
+                type="button"
+                onClick={handleSubmit}
+                className="btn btn-primary mt-5"
+                value="Convert"
+              />
+            </>
+          )}
+          {!isLoading && text && (
+            <>
+              <textarea
+                className="form-control w-100 mt-5"
+                rows="30"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+              ></textarea>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+    
         </div>
     )
 }
